0659037c32ded9c88f6b772d5b85719c4d14666e
Waize-95 <veezobutt@gmail.com>
2026-05-03 13:53:54 +0500

    blue_background

--- 24095.asm
+++ 24095.asm
@@ -1,6 +1,7 @@
 org 100h
 
 call clscr
+call blue_background
 jmp exit
 
 
@@ -28,6 +29,31 @@ return_clscr:   pop di
                 pop ax
                 pop bp
                 ret
+
+
+blue_background:    push bp
+                    mov bp,sp
+                    push ax
+                    push di
+                    push es
+                    xor di,di
+                    mov ax,0xb800
+                    mov es,ax
+                    mov ax,1020h
+                    jmp background_loop
+
+background_loop:    mov [es:di],ax
+                    add di,2
+                    cmp di,4000
+                    jnz background_loop
+                    jmp ret_back
+
+
+ret_back:   pop es
+            pop di
+            pop ax
+            pop bp
+            ret
 
 
 
