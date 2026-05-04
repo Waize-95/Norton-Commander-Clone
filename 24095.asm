7ec468f10cf73648b5664e65eee7711a48aaf010
Waize-95 <veezobutt@gmail.com>
2026-05-04 23:07:59 +0500

    fully_keyboard_control

--- 24095.asm
+++ 24095.asm
@@ -162,24 +162,133 @@ fix_boundry:    mov ax,0xb800
                 mov [es:di],ax
 
 
-
-xor ax,ax
-mov al,[active_pane]
-push ax
-mov al,[current_row]
-push ax
-mov al,[l_start_cols]
-push ax
-mov al,[l_end_cols]
-push ax
-mov al,[r_start_cols]
-push ax
-mov al,[r_end_cols]
-push ax
-call highlight_row
+;=================================================================================================================
+; the above was just initialization of the whole UI
 
 
-jmp exit
+
+main_highlight: xor ax,ax
+                mov al,[active_pane]
+                push ax
+                mov al,[current_row]
+                push ax
+                mov al,[l_start_cols]
+                push ax
+                mov al,[l_end_cols]
+                push ax
+                mov al,[r_start_cols]
+                push ax
+                mov al,[r_end_cols]
+                push ax
+                call highlight_row
+                jmp main_loop
+
+
+main_unhighlight: 
+
+
+
+main_loop:      mov ah,00h      ;waiting for the user to press the key
+                int 16h
+
+                cmp al,1Bh        ; if it is escape, just exit
+                je near exit
+
+                cmp al,09h      ; if it is tab swictn panes
+                je switch_panes
+
+                cmp ah,48h      ; non extended keys like tab and escape are captured by AL else extended keys like
+                je move_up      ; arrow keys are captured by AH
+                      
+                cmp ah,50h
+                je move_down
+
+                jmp main_loop   ;if any other key is pressed just wait again
+
+
+;IDEA instead of just copying and pasting unhighlight everywhere, i can send a value in al like 0,1,2
+; after calling the unhighligh_row i compare the value of al, if it is 0 i jump to switch panes etc
+  
+switch_panes:   xor ax,ax
+                mov al,[active_pane]
+                push ax
+                mov al,[current_row]
+                push ax
+                mov al,[l_start_cols]
+                push ax
+                mov al,[l_end_cols]
+                push ax
+                mov al,[r_start_cols]
+                push ax
+                mov al,[r_end_cols]
+                push ax
+                call unhighlight_row
+                xor byte [active_pane],1
+                jmp main_highlight
+
+move_up:        xor ax,ax
+                mov al,[active_pane]
+                push ax
+                mov al,[current_row]
+                push ax
+                mov al,[l_start_cols]
+                push ax
+                mov al,[l_end_cols]
+                push ax
+                mov al,[r_start_cols]
+                push ax
+                mov al,[r_end_cols]
+                push ax
+                call unhighlight_row
+                mov al,[current_row]
+                cmp al,2
+                je first_row
+                dec al
+                mov [current_row],al
+                jmp main_highlight
+
+first_row:      mov al,21
+                mov [current_row],al
+                jmp main_highlight
+
+
+move_down:      xor ax,ax
+                mov al,[active_pane]
+                push ax
+                mov al,[current_row]
+                push ax
+                mov al,[l_start_cols]
+                push ax
+                mov al,[l_end_cols]
+                push ax
+                mov al,[r_start_cols]
+                push ax
+                mov al,[r_end_cols]
+                push ax
+                call unhighlight_row
+                mov al,[current_row]
+                cmp al,21       ;last row that can be accessed
+                je last_row
+                inc al
+                mov [current_row],AL
+                jmp main_highlight
+
+last_row:       mov al,2 ; first accessible row
+                mov [current_row],al
+                jmp main_highlight
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -606,15 +715,7 @@ return_unhighlight:       pop es
 
 
 
-
-
-
-
-
-
-
-exit:   mov ah, 00h
-        int 16h
+exit:   
         mov ax,4c00h
         int 21h
 
@@ -677,11 +778,10 @@ dest_col: db 78
 
 ;===============================================================================================================
 
-active_pane: db 1
+active_pane: db 0
 current_row: db 2
 l_start_cols: db 1
 l_end_cols: db 38
 r_start_cols: db 41
 r_end_cols: db 78
 
-previous_row: db 1
